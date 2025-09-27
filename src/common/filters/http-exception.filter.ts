0efd41e946b03ca47ccf4df46bf989f8d7f98a0b
Felipe Lima <lipezera1836@gmail.com>
2025-09-27 01:53:45 -0300

    setup: http exception filter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentsHost,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { error } from 'console';
+import path from 'path';
+
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const request = context.getRequest();
+    const response = context.getResponse();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    response.status(status).json({
+      timestamps: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter());
   app.enableCors({
-    origin: [
-      process.env.CORS_ORIGINS,
-    ],
+    origin: [process.env.CORS_ORIGINS],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Authorization']
-  })
+    exposedHeaders: ['Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
