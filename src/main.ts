75b5c1e7d87098e32e41d867fc9b9c45fd2e227e
Felipe Lima <lipezera1836@gmail.com>
2025-09-27 01:58:34 -0300

    feat: timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+
   app.enableCors({
     origin: [process.env.CORS_ORIGINS],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Authorization'],
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
