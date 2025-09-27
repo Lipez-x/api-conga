03774cb2f2b021a9a648590bd5c41ea10813b8b9
Felipe Lima <lipezera1836@gmail.com>
2025-09-27 00:09:59 -0300

    setup: configuração do CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      process.env.CORS_ORIGINS,
+    ],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization']
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
