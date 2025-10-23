4c3ec7f6121a6a7366068e641a7a61b3632b93dc
Felipe Lima <lipezera1836@gmail.com>
2025-10-23 02:10:20 -0300

    refactor: tratamento de datas na em personnel entity

--- src/personnel-cost/dtos/register-personnel-cost.dto.ts
+++ src/personnel-cost/dtos/register-personnel-cost.dto.ts
@@ -8,7 +8,7 @@ import {
 } from 'class-validator';
 import { CostType } from '../enums/cost-type.enum';
 
-export class RegisterPersonnelCost {
+export class RegisterPersonnelCostDto {
   @IsEnum(CostType)
   @IsNotEmpty()
   type: CostType;
--- src/personnel-cost/entities/personnel-cost.entity.ts
+++ src/personnel-cost/entities/personnel-cost.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { CostType } from '../enums/cost-type.enum';
 
-@Entity('personnel-cost')
+@Entity('personnel_cost')
 export class PersonnelCost {
   @PrimaryGeneratedColumn('uuid')
   id: string;
@@ -9,12 +9,12 @@ export class PersonnelCost {
   @Column({ type: 'enum', enum: CostType })
   type: CostType;
 
-  @Column({ type: 'time without time zone', nullable: false })
+  @Column({ type: 'date', nullable: false })
   date: Date;
 
   @Column({ type: 'decimal', precision: 12, scale: 2, nullable: false })
   value: string;
 
   @Column({ type: 'text', nullable: true })
-  description: string;
+  description?: string;
 }
