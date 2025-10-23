781471f5ba51f8a2223b5b496e21525a5be796cb
Felipe Lima <lipezera1836@gmail.com>
2025-10-23 02:05:33 -0300

    feat: implementado registro de despesa com pessoal

--- src/personnel-cost/personnel-cost.controller.ts
+++ src/personnel-cost/personnel-cost.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PersonnelCostService } from './personnel-cost.service';
+import { RegisterPersonnelCostDto } from './dtos/register-personnel-cost.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/users/enums/user-role.enum';
 
+@Roles(UserRole.ADMIN)
 @Controller('personnel-cost')
 export class PersonnelCostController {
   constructor(private readonly personnelCostService: PersonnelCostService) {}
+
+  @Post('/register')
+  async register(@Body() registerPersonnelCostDto: RegisterPersonnelCostDto) {
+    return await this.personnelCostService.register(registerPersonnelCostDto);
+  }
 }
