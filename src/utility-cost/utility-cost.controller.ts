88fa64ad2b7a3f8a6fdc684fbd9efdd00790512d
Felipe Lima <lipezera1836@gmail.com>
2025-10-27 20:59:36 -0300

    feat: implementado registro de utility-cost

--- src/utility-cost/utility-cost.controller.ts
+++ src/utility-cost/utility-cost.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { RegisterUtilityCostDto } from './dtos/register-utility-cost.dto';
+import { UtilityCostService } from './utility-cost.service';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/users/enums/user-role.enum';
 
+@Roles(UserRole.ADMIN)
 @Controller('utility-cost')
-export class UtilityCostController {}
+export class UtilityCostController {
+  constructor(private readonly utilityCostService: UtilityCostService) {}
+
+  @Post('/register')
+  @UsePipes(ValidationPipe)
+  async register(@Body() registerUtilityCostDto: RegisterUtilityCostDto) {
+    return await this.utilityCostService.register(registerUtilityCostDto);
+  }
+}
--- src/utility-cost/utility-cost.service.ts
+++ src/utility-cost/utility-cost.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { RegisterUtilityCostDto } from './dtos/register-utility-cost.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UtilityCost } from './entities/utility-cost.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UtilityCostService {}
+export class UtilityCostService {
+  private logger = new Logger(UtilityCostService.name);
+  constructor(
+    @InjectRepository(UtilityCost)
+    private readonly utilityCostRepository: Repository<UtilityCost>,
+  ) {}
+
+  async register(registerUtilityCostDto: RegisterUtilityCostDto) {
+    try {
+      const utilityCost = this.utilityCostRepository.create({
+        ...registerUtilityCostDto,
+      });
+
+      return await this.utilityCostRepository.save(utilityCost);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
