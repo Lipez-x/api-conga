0621f5d15e32b976248addcf0fbe8a0f2d9275ec
Lizandra Pinheiro <lizandra.pinheiro@alu.ufc.br>
2025-10-31 19:53:36 -0300

    feat: implementando registro de operational-cost

--- src/operational-cost/operational-cost.controller.ts
+++ src/operational-cost/operational-cost.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { OperationalCostService } from './operational-cost.service';
+import { RegisterOperationalCostDto } from './dtos/register-operational-cost.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/users/enums/user-role.enum';
 
+@Roles(UserRole.ADMIN)
 @Controller('operational-cost')
-export class OperationalCostController {}
+export class OperationalCostController {
+  constructor(
+    private readonly operationalCostService: OperationalCostService,
+  ) {}
+
+  @Post('/register')
+  @UsePipes(ValidationPipe)
+  async register(
+    @Body() registerOperationalCostDto: RegisterOperationalCostDto,
+  ) {
+    return await this.operationalCostService.register(
+      registerOperationalCostDto,
+    );
+  }
+}
--- src/operational-cost/operational-cost.service.ts
+++ src/operational-cost/operational-cost.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OperationalCost } from './entities/operational-cost.entity';
+import { Repository } from 'typeorm';
+import { RegisterOperationalCostDto } from './dtos/register-operational-cost.dto';
 
 @Injectable()
-export class OperationalCostService {}
+export class OperationalCostService {
+  private logger = new Logger(OperationalCostService.name);
+  constructor(
+    @InjectRepository(OperationalCost)
+    private readonly operationalCostRepository: Repository<OperationalCost>,
+  ) {}
+  async register(registerOperationalCostDto: RegisterOperationalCostDto) {
+    try {
+      const operationalCost = this.operationalCostRepository.create({
+        ...registerOperationalCostDto,
+      });
+
+      return await this.operationalCostRepository.save(operationalCost);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
