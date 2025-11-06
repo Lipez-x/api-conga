17c11dd5da10e3799d12b92dd3feae90b613b6d4
Felipe Lima <lipezera1836@gmail.com>
2025-11-06 07:54:01 -0300

    chore: campos de data em sale-price como timestamps

--- src/database/migrations/1762422442063-sale-price-schema.ts
+++ src/database/migrations/1762422442063-sale-price-schema.ts
@@ -4,7 +4,7 @@ export class SalePriceSchema1762422442063 implements MigrationInterface {
     name = 'SalePriceSchema1762422442063'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "sale_price" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "value" numeric(10,2) NOT NULL, "startDate" date NOT NULL, "endDate" date NOT NULL, CONSTRAINT "PK_16924f33c9cec7a2034f07073bc" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "sale_price" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "value" numeric(10,2) NOT NULL, "startDate" TIMESTAMP NOT NULL, "endDate" TIMESTAMP NOT NULL, CONSTRAINT "PK_16924f33c9cec7a2034f07073bc" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
