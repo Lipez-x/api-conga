13ad50d6b1b55fb820c9196690818f232c2b5ae6
Felipe Lima <lipezera1836@gmail.com>
2025-11-10 04:37:40 -0300

    chore: modularização de producer-production

---
+++ src/productions/producer/producer-production.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('producer-production')
+export class ProducerProductionController {}
--- src/productions/productions.module.ts
+++ src/productions/productions.module.ts
@@ -3,10 +3,12 @@ import { LocalProductionService } from './local/local-production.service';
 import { LocalProductionController } from './local/local-production.controller';
 import { LocalProduction } from './local/entities/local-production.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProducerProductionController } from './producer/producer-production.controller';
+import { ProducerProductionService } from './producer/producer-production.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([LocalProduction])],
-  controllers: [LocalProductionController],
-  providers: [LocalProductionService],
+  controllers: [LocalProductionController, ProducerProductionController],
+  providers: [LocalProductionService, ProducerProductionService],
 })
 export class ProductionsModule {}
