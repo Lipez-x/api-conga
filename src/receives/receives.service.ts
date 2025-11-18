5811aaad737ba3173daed6fffa06f2856ee2e0e6
Felipe Lima <lipezera1836@gmail.com>
2025-11-18 04:17:56 -0300

    feat: implementa a busca e criação de uma nova receita

--- src/receives/receives.service.ts
+++ src/receives/receives.service.ts
@@ -1,4 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Receive } from './entities/receive.entity';
+import { Repository } from 'typeorm';
+import { SalePriceService } from 'src/sale-price/sale-price.service';
 
 @Injectable()
-export class ReceivesService {}
+export class ReceivesService {
+  private logger = new Logger(ReceivesService.name);
+  constructor(
+    @InjectRepository(Receive)
+    private readonly receiveRepository: Repository<Receive>,
+    private readonly salePriceService: SalePriceService,
+  ) {}
+
+  async findOrCreate(date: Date) {
+    try {
+      const receive = await this.receiveRepository.findOne({
+        where: { date },
+        relations: ['localProductions', 'producerProductions'],
+      });
+
+      if (!receive) {
+        return await this.create(date);
+      }
+
+      return receive;
+    } catch (error) {
+      this.logger.error(error.message);
+
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async create(date: Date) {
+    try {
+      const salePrice = await this.salePriceService.getByDate(date);
+
+      if (!salePrice) {
+        throw new NotFoundException('Preço do leite não encontrado');
+      }
+
+      const receive = this.receiveRepository.create({
+        date: date,
+        salePrice: salePrice.value,
+        localProductions: [],
+        producerProductions: [],
+      });
+
+      return receive;
+    } catch (error) {
+      this.logger.error(error.message);
+
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async save(receive: Receive) {
+    try {
+      const totalLocal = receive.localProductions.reduce(
+        (sum, lp) => sum + Number(lp.grossQuantity - lp.consumedQuantity),
+        0,
+      );
+
+      const totalProducer = receive.producerProductions.reduce(
+        (sum, pp) => sum + Number(pp.totalQuantity),
+        0,
+      );
+
+      receive.tankQuantity = totalLocal + totalProducer;
+
+      return await this.receiveRepository.save(receive);
+    } catch (error) {
+      this.logger.error(error.message);
+
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
