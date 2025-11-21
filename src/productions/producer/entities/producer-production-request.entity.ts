5c0d637988950b817bc087104bc0b6e70059efa1
Felipe Lima <lipezera1836@gmail.com>
2025-11-21 07:00:54 -0300

    feat: request de producer-production mapeada para a base de dados

---
+++ src/productions/producer/entities/producer-production-request.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RequestStatus } from '../enums/request-status.enum';
+
+@Entity('producer_production_request')
+export class ProducerProduction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @Column({ name: 'producer_name', type: 'text' })
+  producerName: string;
+
+  @Column({
+    name: 'total_quantity',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  totalQuantity: number;
+
+  @Column({ type: 'enum', enum: RequestStatus })
+  status: RequestStatus;
+}
