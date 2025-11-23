4f8d9a6f2b339f5fe89d89c76e19ead8c74312ff
Felipe Lima <lipezera1836@gmail.com>
2025-11-22 21:30:05 -0300

    fix: retira dependência de local-production do getDailyProduction

--- src/productions/productions.service.ts
+++ src/productions/productions.service.ts
@@ -15,23 +15,33 @@ export class ProductionsService {
   async getDailyProduction(filters: GetDailyProductionDto) {
     const { dateFrom, dateTo } = filters;
 
+    const sub =
+      this.dataSource
+        .createQueryBuilder()
+        .select('l.date', 'date')
+        .from(LocalProduction, 'l')
+        .getQuery() +
+      `
+    UNION
+    ` +
+      this.dataSource
+        .createQueryBuilder()
+        .select('pp.date', 'date')
+        .from(ProducerProduction, 'pp')
+        .getQuery();
+
     const query = this.dataSource
       .createQueryBuilder()
-      .select('l.date', 'date')
-      .addSelect('SUM(l.gross_quantity)', 'grossQuantity')
-      .addSelect('SUM(l.consumed_quantity)', 'consumedQuantity')
-      .addSelect('SUM(l.total_quantity)', 'totalQuantity')
-      .addSelect(
-        (qb) =>
-          qb
-            .select('COALESCE(SUM(pp.total_quantity), 0)')
-            .from(ProducerProduction, 'pp')
-            .where('pp.date = l.date'),
-        'totalProducers',
-      )
-      .from(LocalProduction, 'l')
-      .groupBy('l.date')
-      .orderBy('l.date', 'DESC');
+      .select('d.date', 'date')
+      .addSelect('COALESCE(SUM(l.gross_quantity), 0)', 'grossQuantity')
+      .addSelect('COALESCE(SUM(l.consumed_quantity), 0)', 'consumedQuantity')
+      .addSelect('COALESCE(SUM(l.total_quantity), 0)', 'totalQuantity')
+      .addSelect('COALESCE(SUM(pp.total_quantity), 0)', 'totalProducers')
+      .from(`(${sub})`, 'd')
+      .leftJoin(LocalProduction, 'l', 'l.date = d.date')
+      .leftJoin(ProducerProduction, 'pp', 'pp.date = d.date')
+      .groupBy('d.date')
+      .orderBy('d.date', 'DESC');
 
     if (dateFrom) query.andWhere('l.date >= :dateFrom', { dateFrom });
     if (dateTo) query.andWhere('l.date <= :dateTo', { dateTo });
