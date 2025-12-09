17b6e2c03af3c13b5bea9fc684d9980a29813dc0
Felipe Lima <lipezera1836@gmail.com>
2025-12-09 14:21:14 -0300

    chore: registra pdf service como provider de financial-report

--- src/financial-report/financial-report.module.ts
+++ src/financial-report/financial-report.module.ts
@@ -4,11 +4,12 @@ import { FinancialReportService } from './financial-report.service';
 import { ExpensesModule } from 'src/expenses/expenses.module';
 import { ReceivesModule } from 'src/receives/receives.module';
 import { ProductionsModule } from 'src/productions/productions.module';
+import { PdfService } from './financial-report-pdf.service';
 
 @Module({
   imports: [ExpensesModule, ReceivesModule, ProductionsModule],
   controllers: [FinancialReportController],
-  providers: [FinancialReportService],
+  providers: [FinancialReportService, PdfService],
   exports: [FinancialReportService],
 })
 export class FinancialReportModule {}
